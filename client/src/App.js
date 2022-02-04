58d38c239d5e3d0d978d8d3d908b6f477f8cc39a
ritik <ritikagarwal470@gmail.com>
2022-02-04 09:21:50 +0530

    adding form ui

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./App.css");
+function App() {
+    return Login < /h1>
+        < form >
+        type;
+    "text";
+    placeholder = "Name" > /input>
+        < input;
+    type = "text";
+    placeholder = "email" > /input>
+        < input;
+    type = "passsword";
+    placeholder = "password" > /input>
+        < /form>
+        < /div>;
+}
+exports.default = App;
+//# sourceMappingURL=App.js.map
