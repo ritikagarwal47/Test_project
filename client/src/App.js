da5d4f072284626ac37f9b83a617e84579e1768e
ritik <ritikagarwal470@gmail.com>
2022-02-08 09:49:00 +0530

    login/register

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,10 @@ function App() {
     {/* <BrowserRouter></BrowserRouter> */}
     <h1>Login</h1>
     <form>
-      <input type="text" placeholder="Name"></input>
-      <input type="text" placeholder="email"></input>
-      <input type="passsword" placeholder="password"></input>
+      <input type="text" placeholder="Name"></input><br/>
+      <input type="text" placeholder="email"></input><br/>
+      <input type="passsword" placeholder="password"></input><br/>
+      <input type="submit" value="Register"></input>
     </form>
   </div>
 }
